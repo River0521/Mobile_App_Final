41fce30c7a4c7d3035399404ce7558251f4dd06f
Zae <justinthomas52199@gmail.com>
2023-07-19 21:43:39 -0400

    HomeScreen update

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,15 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
+import { FontAwesome } from '@expo/vector-icons';
+
 
 export const HomeScreen = () => {
     return (
-        <Text className="flex-1">HomeScreen</Text>
+        <View>
+            <Text className="text-center text-4xl mt-10">Welcome, username!</Text>
+            <Text className="text-center text-2xl mt-1">Your current HighScore is: 0 Steps!</Text>
+            <TouchableOpacity className=" flex-auto bg-lime-200 mt-2 items-center mx-32 my-3 align-middle"><Text className="align-middle"><FontAwesome name="heartbeat" size={24} color="#449DD1" className="mr-4" />Start Session!</Text></TouchableOpacity>
+        </View>
+
     )
 }
