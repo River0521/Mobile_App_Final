070447557354a5f04dd82910f62ec6cbefed515b
Zae <justinthomas52199@gmail.com>
2023-07-19 22:08:07 -0400

    added modal for login screen & added login-> home

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
-import { StyleSheet, Text, View } from "react-native";
-import { Login } from "./screens/Login";
-//import { AppNavigation } from "./navigation/appNavigation";
+import { Modal, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { HomeScreen } from "./screens/HomeScreen";
 import { Leaderboard } from "./screens/Leaderboard";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import { Entypo, AntDesign } from '@expo/vector-icons';
 
+
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+
   return (
+
     <NavigationContainer>
       <Tab.Navigator className="mb-4">
         <Tab.Screen name="Home Screen"
@@ -24,14 +25,7 @@ export default function App() {
           component={Leaderboard} />
       </Tab.Navigator>
     </NavigationContainer>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Text, TouchableOpacity, View } from 'react-native'
 import { FontAwesome } from '@expo/vector-icons';
+import { Login } from './Login';
 
 
 export const HomeScreen = () => {
     return (
         <View>
+
+
             <Text className="text-center text-4xl mt-10">Welcome, username!</Text>
             <Text className="text-center text-2xl mt-1">Your current HighScore is: 0 Steps!</Text>
             <TouchableOpacity className=" flex-auto bg-lime-200 mt-2 items-center mx-32 my-3 align-middle"><Text className="align-middle"><FontAwesome name="heartbeat" size={24} color="#449DD1" className="mr-4" />Start Session!</Text></TouchableOpacity>
+
+            <Login className="mt-50" />
         </View>
 
     )
