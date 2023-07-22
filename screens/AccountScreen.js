a8d49ca13182fc913bd25ab8c8a25c8f41380190
Zae <justinthomas52199@gmail.com>
2023-07-22 01:39:43 -0400

    change account layout, i liked this one better

--- screens/AccountScreen.js
+++ screens/AccountScreen.js
@@ -12,30 +12,34 @@ export const AccountScreen = () => {
         width: "100%",
       }}
     >
-      <Text className="text-2xl -mt-40">Account Session Information</Text>
-      <Text className="text-4xl mt-20">UserName</Text>
+      <Text className="text-2xl -mt-40 text-center">
+        Account Session Information
+      </Text>
+      <Text className="text-4xl mt-20 text-center">UserName</Text>
 
       <View>
         <View className="mt-10">
-          <Text className="mb-3 text-2xl" style={{ fontWeight: "bold" }}>
+          <Text
+            className="mb-3 text-2xl text-center"
+            style={{ fontWeight: "bold" }}
+          >
             Highest Scores:
           </Text>
-          <Text className="text-xl text-center">Highest Steps: 0</Text>
-          <Text className="text-xl text-center">Highest Distance: 0</Text>
-          <Text className="text-xl text-center">
-            Highest Calories Burned: 0
-          </Text>
+          <Text className="text-xl">Highest Steps: 0</Text>
+          <Text className="text-xl">Highest Distance: 0</Text>
+          <Text className="text-xl">Highest Calories Burned: 0</Text>
         </View>
 
         <View className="mt-10">
-          <Text className="mb-3 text-2xl" style={{ fontWeight: "bold" }}>
+          <Text
+            className="mb-3 text-2xl text-center"
+            style={{ fontWeight: "bold" }}
+          >
             Last Session Scores:
           </Text>
-          <Text className="text-xl text-center">Last Session Steps: 0</Text>
-          <Text className="text-xl text-center">Last Sessio Distance: 0</Text>
-          <Text className="text-xl text-center">
-            Last Sessio Calories Burned: 0
-          </Text>
+          <Text className="text-xl">Last Session Steps: 0</Text>
+          <Text className="text-xl">Last Sessio Distance: 0</Text>
+          <Text className="text-xl">Last Sessio Calories Burned: 0</Text>
         </View>
       </View>
     </View>
